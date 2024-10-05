461d41ac34f45490b4ade83dba96c40e5b28eceb
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-10-04 19:04:50 -0600

    ci: add new libraries and minimal settings

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from lexico import get_all_tokens
-from sintactico import parse_for_loop
+from combined import get_analyzer
 
 app = Flask(__name__)
 CORS(app)  # Permite CORS para todas las rutas
@@ -9,14 +9,27 @@ CORS(app)  # Permite CORS para todas las rutas
 @app.route('/analyze', methods=['POST'])
 def analyze():
     code = request.json['code']
+    language = request.json['language']  # Nuevo parámetro para el lenguaje
     
     # Análisis léxico
     tokens = get_all_tokens(code)
-    token_list = [{"token": t[0], "lexeme": str(t[1]), "line": t[2]} for t in tokens]
-    
+    token_list = [
+        {
+            "token": t[0],
+            "lexeme": str(t[1]),
+            "wordReserv": t[2],
+            "identifier": t[3],
+            "cadena": t[4],
+            "numero": t[5],
+            "simbolo": t[6],
+            "line": t[7]
+        }
+        for t in tokens
+    ]
+
     # Análisis sintáctico
-    parse_for_loop(code)
-    from sintactico import errors as syntax_errors
+    analyzer = get_analyzer(language)
+    syntax_errors = analyzer.parse(code)
     
     return jsonify({
         "tokens": token_list,
